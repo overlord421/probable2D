d7ccfc63803d2fcad9395db47204c1d5b527daa0
leon <nema>
2026-04-22 08:02:48 +0300

    график потока энергии

--- plot_flux.py
+++ plot_flux.py
@@ -3,3 +3,10 @@ import numpy as np
 
 
 flux = np.loadtxt('output/energy_flux_avg.txt')
+
+# matplotlib.rcParams.update({'font.size': 24})
+plt.figure(figsize=(12, 8))
+plt.plot(flux)
+plt.title('Поток энергии (не удельный)')
+plt.grid()
+plt.show()
